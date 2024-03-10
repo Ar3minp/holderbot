cab8463d4e6aad58199d2e4915257ff276e5ed96
Armin <130553205+Ar3minp@users.noreply.github.com>
2024-03-10 21:26:43 +0330

    Update holderbot.sh

--- holderbot.sh
+++ holderbot.sh
@@ -78,7 +78,7 @@ fi
 mkdir holderbot
 cd holderbot
 
-git clone -b main https://github.com/erfjab/holderbot.git .
+git clone -b main https://github.com/Ar3minp/holderbot.git .
 
 sudo apt install -y python3.10-venv
 python3 -m venv hold
